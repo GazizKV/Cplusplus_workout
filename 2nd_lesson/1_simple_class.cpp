727b9db803abdc6d19839b9f4d8fcd02d5e4bf88
gazizkv <valitovgaziz@gmail.com>
2020-09-11 08:16:22 +0500

    polimorfism and tamplate in fn

--- 2nd_lesson/1_simple_class.cpp
+++ 2nd_lesson/1_simple_class.cpp
@@ -3,17 +3,28 @@
 
 struct Student
 {
+private:
 	std::string name;
 	std::string group;
 	int age;
+	double *memory;
 
-
-	void init()
+public:
+	Student(std::string name_, std::string group_, int age_)
 	{
-		name = "Vasya";
-		group = "B02-999";
-		age = 18;
+		name = name_;
+		group = group_;
+		age = age_;
+		std::cout << "Hooray!!! Me " << name << " dismissed enrolled to the course!\n";
+		//RESOURCE ALLOCATION
+		memory = new double[100];
+	}
 
+	~Student()
+	{
+		std::cout << "Ouch!!! Me " << name << " from the course!\n";
+		//RESOURCE DEALLOCATION
+		delete[] memory;
 	}
 
 	void print() const
@@ -26,15 +37,20 @@ struct Student
 	{
 		std::cout << "Hooray!!! Today is my birthday! I'm " << name << "!\n";
 		age += 1;
+		print(); //the same as this->print()
 	}
 };
 int main()
 {
-	Student a;
+	Student a("Petya", "B02-991", 18);
 
-	a.init();
 	a.print();
 	a.ageing();
-	a.print();
+
+	Student b("Vasya", "B03-993", 35);
+	b.print();
+	b.ageing();
+
+
 	return 0;
 }
---
+++ 2nd_lesson/2_overloading.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+void foo(unsigned char x)
+{
+	std::cout << " foo(unsigned char) is called\n";	
+}
+void foo(int x)
+{
+	std::cout << " foo(int) is called\n";
+}
+
+void foo(double x)
+{
+	std::cout << " foo(double) is called\n";	
+}
+
+int main()
+{
+	foo('A');
+	foo(1);
+	foo(4.5);
+	foo(23U);
+
+	return 0;
+}
