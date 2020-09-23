8bb1f8ee575d83300b6907bce9d3e7483f6d62aa
gazizkv <valitovgaziz@gmail.com>
2020-09-23 11:20:50 +0500

    recursion on hanoi

---
+++ 9th_lesson_rexursion/euclid.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b)
+{
+	return (b == 0) ? a : gcd(b, a%b);
+}
+
+int main()
+{
+	int a, b;
+	std::cin >> a >> b;
+	std::cout << gcd(a, b) << '\n';
+
+	return 0;
+}
---
+++ 9th_lesson_rexursion/math_pow.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+ *	Hanoi solution finder.
+ *	param i: start pin number
+ *	param k: finish pin number
+ *	param n: number of disk
+ * */
+
+void hanoi(int i, int k, int n)
+{
+	if (n == 1)
+	{
+		std::cout << "Move disk 1 from pin " << i
+			<< " to pin " << k << ".\n";
+	} else
+	{
+		int tmp = 6 - i -k; // temporary pin
+		hanoi(i, tmp, n -1);
+		std::cout << "Move disk " << n << " from pin " << i
+			<< " to pin " << k << ".\n";
+		hanoi(tmp, k, n - 1);
+
+	}
+
+}
+
+int main()
+{
+	hanoi(1, 2, 4);
+
+	return 0;
+}
